875a2b10409a046b5bfc85a053ce71b507235967
Lab103 <Lab103@LAB101-06>
2018-10-20 11:14:33 -0300

    Aula 20/10/2018 - Caso de teste - Usuário inválido.

---
+++ sce/src/main/java/com/fatec/sce/model/ConfiguraDB.java
@@ -0,0 +1,49 @@
+package com.fatec.sce.model;
+
+public class ConfiguraDB {
+
+	String url = "";
+	String driver = "";
+	String usuario = "";
+	String senha = "";
+
+	public ConfiguraDB(String url, String driver, String usuario, String senha) {
+		this.url = url;
+		this.driver = driver;
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDriver() {
+		return driver;
+	}
+
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
